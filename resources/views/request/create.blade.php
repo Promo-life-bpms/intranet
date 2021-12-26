8d6b5d1e33ec215ad773ca77cf472dd9ccc9cf12
Oscar <krz.oficial@gmail.com>
2021-12-25 19:06:50 -0600

    ajustes al calendario en la pantalla de solicitudes

--- resources/views/request/create.blade.php
+++ resources/views/request/create.blade.php
@@ -70,9 +70,11 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <input type="hidden" id="fechasSeleccionadas">
+                    <div class="mb-2 form-group">
+                    {!! Form::label('days', 'Seleccionar dias') !!}
                     <div id='calendar'></div>
                 </div>
+                <div>
             </div>
             {!! Form::submit('CREAR SOLICITUD', ['class' => 'btnCreate mt-4', 'name'=>'submit']) !!}
         </div>
@@ -81,105 +83,212 @@
 @stop
 
 @section('styles')
-    <link rel="stylesheet" href="{{ asset('assets/vendors/fullcalendar/main.min.css') }}">
-    <style>
-        body {
-            margin: 40px 10px;
-            padding: 0;
-            font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-            font-size: 14px;
-        }
 
-        #calendar {
-            max-width: 600px;
-            margin: 0 auto;
-            color: black;
-        }
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
 
-       
-    </style>
+<style>
+    body {
+        margin: 40px 10px;
+        padding: 0;
+        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+        font-size: 14px;
+    }
+
+    #calendar{
+        width: 100%;
+    }
+
+    #calendar h2{
+        font-size: 12px;
+    }
+    #calendar a{
+        margin: 0 auto;
+        font-size: 16px;
+        color: #ffffff;
+    }
+
+
+   
+</style>
 @stop
 
 @section('scripts')
-    <script src="{{ asset('assets/vendors/fullcalendar/main.min.js') }}"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            
-            let noworkingdays = @json($noworkingdays)
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script>
+    $(document).ready(function () {
+       
+    var SITEURL = "{{ url('/') }}";
+      
+    $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+      
 
-            events = []
-            noworkingdays.forEach(element => {
-                events.push({
-                    title: element.reason,
-                    start: element.day,
-                    type: 'noworking',
-                    display: 'background',
-                    editable: false,
-                })
-            });
+    let noworkingdays = @json($noworkingdays)
 
-            let dateActual = moment().format('YYYY-MM-DD');
-            const fechasSeleccionadasEl = document.querySelector('#fechasSeleccionadas')
-            var calendarEl = document.getElementById('calendar');
-            var daysSelecteds = new Set();
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialDate: dateActual,
-                selectable: true,
-                select: function(arg) {
-                    var fecha1 = moment(arg.startStr);
-                    var fecha2 = moment(arg.endStr);
-                    dateActual = moment(dateActual)
-                    if (fecha2.diff(fecha1, 'days') == 1) {
-                        if (fecha1.diff(dateActual, 'days') > 0) {
-                            if (!daysSelecteds.has(arg.startStr)) {
-                                if (fecha1.isoWeekday() !== 6 && fecha1.isoWeekday() !== 7) {
-                                    var r = confirm("Seleccionar este dia");
-                                    if (r) {
-                                        calendar.addEvent({
-                                            title: 'OK',
-                                            start: arg.start,
-                                            startStr: arg.startStr,
-                                            end: arg.end,
-                                            allDay: arg.allDay
-                                        })
-                                        daysSelecteds = actualizarFechas(calendar.getEvents())
-                                        let lista = [...daysSelecteds]
-                                        alert(lista)
-                                        fechasSeleccionadasEl.value = lista.toString()
+    events = []
+    noworkingdays.forEach(element => {
+        events.push({
+            title: element.reason,
+            start: element.day,
+            rendering: 'background',
+            editable: false,
+            eventStartEditable:false,
+        })
+    });
+    
 
-                                    }
-                                } else {
-                                    alert('No puedes seleccionar el fin de semana')
+  
+
+
+    let dateActual = moment().format('YYYY-MM-DD');
+    const fechasSeleccionadasEl = document.querySelector('#fechasSeleccionadas')
+    var calendarEl = document.getElementById('calendar');
+    var daysSelecteds = new Set();
+
+    var calendar = $('#calendar').fullCalendar({
+                        editable: true,
+                        events: SITEURL + "/events",
+                        displayEventTime: false,
+                        allDay: true,
+                        events,
+                        selectable: true,
+                        selectHelper: true,
+                        select: function (start, end, allDay) {    
+                            //Valida si selecciona un dia festivo
+                            var dates = start.format('YYYY-MM-DD');
+                            var check=false;
+                            
+                            events.forEach(function(e) {
+                            if (dates == e.start){
+                                alert("No puedes seleccionar un día festivo")
+                                throw BreakException
+
+                            }else{
+                                check=true
+                            } 
+                            });
+                            
+                         
+                            if (check==true) {
+                            
+                            check=false
+                            var title = 'Agregado' 
+
+                            var startDate = moment(start),
+                            endDate = moment(end),
+                            date = startDate.clone(),
+                            isWeekend = false;
+                        
+                            while (date.isBefore(endDate)) {
+                            if (date.isoWeekday() == 6 || date.isoWeekday() == 7) {
+                                isWeekend = true;
+                                }    
+                                date.add(1, 'day');
+                            }
+
+                            if (isWeekend) {
+                                alert('No se puede seleccionar fin de semana');
+
+                                return false;
+                            }else{
+                                var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
+                                var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
+                                
+                                if(dateActual<=start){
+                                    
+                                        $.ajax({
+                                        url: SITEURL + "/fullcalenderAjax",
+                                        data: {
+                                            title: title,
+                                            start: start,
+                                            end: end,
+                                            type: 'add',
+                                            
+                                        },
+                                        type: "POST",
+                                        success: function (data) {
+                                            displayMessage("Event Created Successfully");
+        
+                                            calendar.fullCalendar('renderEvent',
+                                                {
+                                                    id: data.id,
+                                                    title: title,
+                                                    start: start,
+                                                    end: end,
+                                                    allDay: allDay,
+                                                    
+                                                },true);
+        
+                                            calendar.fullCalendar('unselect');
+                                            }
+                                        });
+                                        
+                                }else{
+                                    alert('No puedes seleccionar fechas atrasadas ')
                                 }
                             }
+
+                                
+                               
+                            }
+                        },
+                        eventDrop: function (event, delta) {
+                            var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
+                            var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
+      
+                            $.ajax({
+                                url: SITEURL + '/fullcalenderAjax',
+                                data: {
+                                    title: event.title,
+                                    start: start,
+                                    end: end,
+                                    id: event.id,
+                                    type: 'update'
+                                },
+                                type: "POST",
+                                success: function (response) {
+                                    displayMessage("Event Updated Successfully");
+                                }
+                            });
+                        },
+                        eventClick: function (event) {
+                            var deleteMsg = confirm("Do you really want to delete?");
+
+                            
+                            if (deleteMsg) {
+                                $.ajax({
+                                    type: "POST",
+                                    url: SITEURL + '/fullcalenderAjax',
+                                    data: {
+                                            id: event.id,
+                                            type: 'delete'
+                                    },
+                                    success: function (response) {
+                                        calendar.fullCalendar('removeEvents', event.id);
+                                        displayMessage("Event Deleted Successfully");
+                                    }
+                                });
+                            }
                         }
-                        calendar.unselect()
-                    }
-                },
-                eventClick: function(arg) {
-                    if (confirm('Deseas desmarcar este dia?')) {
-                        arg.event.remove()
-                        daysSelecteds = actualizarFechas(calendar.getEvents(), 0)
-                        let lista = [...daysSelecteds]
-                        fechasSeleccionadasEl.value = lista.toString()
-                    }
-                },
-                editable: true,
-                dayMaxEvents: 1, // allow "more" link when too many events
-                events
-            });
+     
+                    });
 
-            calendar.render();
-            calendar.setOption('locale', 'mx');
-        });
-
-        function actualizarFechas(events, estado = 1) {
-            days = new Set();
-            events.forEach(element => {
-                days.add(element._def.extendedProps.startStr);
-            });
-            return days
-        }
-    </script>
+    });
+     
+    function displayMessage(message) {
+        toastr.success(message, 'Event');
+    } 
+      
+ </script>
 
 @stop
+
+
