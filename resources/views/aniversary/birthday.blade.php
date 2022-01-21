bb2780d5df1f0b8e0b8ef0dca0a3977991151459
Oscar <krz.oficial@gmail.com>
2022-01-21 02:29:45 -0600

    arreglo pantalla de aniversarios en mostrar imagen de usuario

--- resources/views/aniversary/birthday.blade.php
+++ resources/views/aniversary/birthday.blade.php
@@ -9,8 +9,13 @@
             @foreach ($employees as $employee)
                 <div class="col-md-3">
                     <div class="card" style="width: 200px; height:220px;">
-                        <img src="https://image.freepik.com/free-vector/man-shows-gesture-great-idea_10045-637.jpg"
+                        @if ($employee->user->image !=null)
+                            <img src="{{ asset($employee->user->image )}}"
                             style="width: 100%; height:150px;   object-fit: contain;" class="card-img-top" alt="imagen">
+                        @else 
+                            <img src="https://image.freepik.com/free-vector/man-shows-gesture-great-idea_10045-637.jpg"
+                            style="width: 100%; height:150px;   object-fit: contain;" class="card-img-top" alt="imagen">
+                        @endif
                         <div class="card-body" style="padding-top:0; padding-bottom:0">
                             <p class="card-title text-center" style=" white-space: nowrap; margin-bottom:5px;">
                                 {{ $employee->user->name . ' ' . $employee->user->lastname }}</p>
