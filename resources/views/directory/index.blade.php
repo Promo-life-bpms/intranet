a672ae7ebd235e17a8a4bd3b77bd68ef33b78abd
Oscar-CR <krz.oficial@gmail.com>
2022-02-17 15:50:24 -0600

    cambio de idioma de DataTable y depuracion de css

--- resources/views/directory/index.blade.php
+++ resources/views/directory/index.blade.php
@@ -95,118 +95,12 @@
 
 <style>
 
-table.dataTable th.dt-left,
-table.dataTable td.dt-left {
-  text-align: left;
-}
-table.dataTable th.dt-center,
-table.dataTable td.dt-center,
-table.dataTable td.dataTables_empty {
-  text-align: center;
-}
-table.dataTable th.dt-right,
-table.dataTable td.dt-right {
-  text-align: right;
-}
-table.dataTable th.dt-justify,
-table.dataTable td.dt-justify {
-  text-align: justify;
-}
-table.dataTable th.dt-nowrap,
-table.dataTable td.dt-nowrap {
-  white-space: nowrap;
-}
-table.dataTable thead th.dt-head-left,
-table.dataTable thead td.dt-head-left,
-table.dataTable tfoot th.dt-head-left,
-table.dataTable tfoot td.dt-head-left {
-  text-align: left;
-}
-table.dataTable thead th.dt-head-center,
-table.dataTable thead td.dt-head-center,
-table.dataTable tfoot th.dt-head-center,
-table.dataTable tfoot td.dt-head-center {
-  text-align: center;
-}
-table.dataTable thead th.dt-head-right,
-table.dataTable thead td.dt-head-right,
-table.dataTable tfoot th.dt-head-right,
-table.dataTable tfoot td.dt-head-right {
-  text-align: right;
-}
-table.dataTable thead th.dt-head-justify,
-table.dataTable thead td.dt-head-justify,
-table.dataTable tfoot th.dt-head-justify,
-table.dataTable tfoot td.dt-head-justify {
-  text-align: justify;
-}
-table.dataTable thead th.dt-head-nowrap,
-table.dataTable thead td.dt-head-nowrap,
-table.dataTable tfoot th.dt-head-nowrap,
-table.dataTable tfoot td.dt-head-nowrap {
-  white-space: nowrap;
-}
-table.dataTable tbody th.dt-body-left,
-table.dataTable tbody td.dt-body-left {
-  text-align: left;
-}
-table.dataTable tbody th.dt-body-center,
-table.dataTable tbody td.dt-body-center {
-  text-align: center;
-}
-table.dataTable tbody th.dt-body-right,
-table.dataTable tbody td.dt-body-right {
-  text-align: right;
-}
-table.dataTable tbody th.dt-body-justify,
-table.dataTable tbody td.dt-body-justify {
-  text-align: justify;
-}
-table.dataTable tbody th.dt-body-nowrap,
-table.dataTable tbody td.dt-body-nowrap {
-  white-space: nowrap;
-}
-table.dataTable td.dt-control {
-  text-align: center;
-  cursor: pointer;
-}
-table.dataTable td.dt-control:before {
-  height: 1em;
-  width: 1em;
-  margin-top: -9px;
-  display: inline-block;
-  color: white;
-  border: 0.15em solid white;
-  border-radius: 1em;
-  box-shadow: 0 0 0.2em #444;
-  box-sizing: content-box;
-  text-align: center;
-  text-indent: 0 !important;
-  font-family: "Courier New", Courier, monospace;
-  line-height: 1em;
-  content: "+";
-  background-color: #31b131;
-}
-table.dataTable tr.dt-hasChild td.dt-control:before {
-  content: "-";
-  background-color: #d33333;
-}
- 
-/*
- * Table styles
- */
 table.dataTable {
   width: 100%;
   margin: 0 auto;
   clear: both;
   border-collapse: separate;
   border-spacing: 0;
-  /*
-   * Header and footer styles
-   */
-  /*
-   * Body styles
-   */
 }
 table.dataTable thead th,
 table.dataTable tfoot th {
@@ -232,11 +126,9 @@ table.dataTable thead .sorting_desc,
 table.dataTable thead .sorting_asc_disabled,
 table.dataTable thead .sorting_desc_disabled {
   cursor: pointer;
-  *cursor: hand;
   background-repeat: no-repeat;
   background-position: center right;
 }
-
 table.dataTable tbody tr {
   background-color: #ffffff;
 }
@@ -314,12 +206,15 @@ table.dataTable.display tbody tr.odd.selected > .sorting_3, table.dataTable.orde
 table.dataTable.display tbody tr.even > .sorting_1, table.dataTable.order-column.stripe tbody tr.even > .sorting_1 {
   background-color: #fafafa;
 }
+
 table.dataTable.display tbody tr.even > .sorting_2, table.dataTable.order-column.stripe tbody tr.even > .sorting_2 {
   background-color: #fcfcfc;
 }
+
 table.dataTable.display tbody tr.even > .sorting_3, table.dataTable.order-column.stripe tbody tr.even > .sorting_3 {
   background-color: #fefefe;
 }
+
 table.dataTable.display tbody tr.even.selected > .sorting_1, table.dataTable.order-column.stripe tbody tr.even.selected > .sorting_1 {
   background-color: #acbad5;
 }
@@ -371,9 +266,6 @@ table.dataTable td {
   box-sizing: content-box;
 }
  
-/*
- * Control feature layout
- */
 .dataTables_wrapper {
   position: relative;
   clear: both;
@@ -503,16 +395,235 @@ table.dataTable td {
   height: 0;
 }
  
-
 </style>
 @endsection
 
 @section('scripts')
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
-
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>    
     <script>
         $(document).ready(function() {
-            $('#table-directory').DataTable();
-        });
+            $('#table-directory').DataTable({
+                language: {
+                    "aria": {
+                        "sortAscending": "Activar para ordenar la columna de manera ascendente",
+                        "sortDescending": "Activar para ordenar la columna de manera descendente"
+                    },
+                    "autoFill": {
+                        "cancel": "Cancelar",
+                        "fill": "Rellene todas las celdas con <i>%d<\/i>",
+                        "fillHorizontal": "Rellenar celdas horizontalmente",
+                        "fillVertical": "Rellenar celdas verticalmente"
+                    },
+                    "buttons": {
+                        "collection": "Colección",
+                        "colvis": "Visibilidad",
+                        "colvisRestore": "Restaurar visibilidad",
+                        "copy": "Copiar",
+                        "copyKeys": "Presione ctrl o u2318 + C para copiar los datos de la tabla al portapapeles del sistema. <br \/> <br \/> Para cancelar, haga clic en este mensaje o presione escape.",
+                        "copySuccess": {
+                            "1": "Copiada 1 fila al portapapeles",
+                            "_": "Copiadas %d fila al portapapeles"
+                        },
+                        "copyTitle": "Copiar al portapapeles",
+                        "csv": "CSV",
+                        "excel": "Excel",
+                        "pageLength": {
+                            "-1": "Mostrar todas las filas",
+                            "_": "Mostrar %d filas"
+                        },
+                        "pdf": "PDF",
+                        "print": "Imprimir",
+                        "createState": "Crear Estado",
+                        "removeAllStates": "Borrar Todos los Estados",
+                        "removeState": "Borrar Estado",
+                        "renameState": "Renombrar Estado",
+                        "savedStates": "Guardar Estado",
+                        "stateRestore": "Restaurar Estado",
+                        "updateState": "Actualizar Estado"
+                    },
+                    "infoThousands": ",",
+                    "loadingRecords": "Cargando...",
+                    "paginate": {
+                        "first": "Primero",
+                        "last": "Último",
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    },
+                    "processing": "Procesando...",
+                    "search": "Buscar:",
+                    "searchBuilder": {
+                        "add": "Añadir condición",
+                        "button": {
+                            "0": "Constructor de búsqueda",
+                            "_": "Constructor de búsqueda (%d)"
+                        },
+                        "clearAll": "Borrar todo",
+                        "condition": "Condición",
+                        "deleteTitle": "Eliminar regla de filtrado",
+                        "leftTitle": "Criterios anulados",
+                        "logicAnd": "Y",
+                        "logicOr": "O",
+                        "rightTitle": "Criterios de sangría",
+                        "title": {
+                            "0": "Constructor de búsqueda",
+                            "_": "Constructor de búsqueda (%d)"
+                        },
+                        "value": "Valor",
+                        "conditions": {
+                            "date": {
+                                "after": "Después",
+                                "before": "Antes",
+                                "between": "Entre",
+                                "empty": "Vacío",
+                                "equals": "Igual a",
+                                "not": "Diferente de",
+                                "notBetween": "No entre",
+                                "notEmpty": "No vacío"
+                            },
+                            "number": {
+                                "between": "Entre",
+                                "empty": "Vacío",
+                                "equals": "Igual a",
+                                "gt": "Mayor a",
+                                "gte": "Mayor o igual a",
+                                "lt": "Menor que",
+                                "lte": "Menor o igual a",
+                                "not": "Diferente de",
+                                "notBetween": "No entre",
+                                "notEmpty": "No vacío"
+                            },
+                            "string": {
+                                "contains": "Contiene",
+                                "empty": "Vacío",
+                                "endsWith": "Termina con",
+                                "equals": "Igual a",
+                                "not": "Diferente de",
+                                "startsWith": "Inicia con",
+                                "notEmpty": "No vacío",
+                                "notContains": "No Contiene",
+                                "notEnds": "No Termina",
+                                "notStarts": "No Comienza"
+                            },
+                            "array": {
+                                "equals": "Igual a",
+                                "empty": "Vacío",
+                                "contains": "Contiene",
+                                "not": "Diferente",
+                                "notEmpty": "No vacío",
+                                "without": "Sin"
+                            }
+                        },
+                        "data": "Datos"
+                    },
+                    "searchPanes": {
+                        "clearMessage": "Borrar todo",
+                        "collapse": {
+                            "0": "Paneles de búsqueda",
+                            "_": "Paneles de búsqueda (%d)"
+                        },
+                        "count": "{total}",
+                        "emptyPanes": "Sin paneles de búsqueda",
+                        "loadMessage": "Cargando paneles de búsqueda",
+                        "title": "Filtros Activos - %d",
+                        "countFiltered": "{shown} ({total})",
+                        "collapseMessage": "Colapsar",
+                        "showMessage": "Mostrar Todo"
+                    },
+                    "select": {
+                        "cells": {
+                            "1": "1 celda seleccionada",
+                            "_": "%d celdas seleccionadas"
+                        },
+                        "columns": {
+                            "1": "1 columna seleccionada",
+                            "_": "%d columnas seleccionadas"
+                        }
+                    },
+                    "thousands": ",",
+                    "datetime": {
+                        "previous": "Anterior",
+                        "hours": "Horas",
+                        "minutes": "Minutos",
+                        "seconds": "Segundos",
+                        "unknown": "-",
+                        "amPm": [
+                            "am",
+                            "pm"
+                        ],
+                        "next": "Siguiente",
+                        "months": {
+                            "0": "Enero",
+                            "1": "Febrero",
+                            "10": "Noviembre",
+                            "11": "Diciembre",
+                            "2": "Marzo",
+                            "3": "Abril",
+                            "4": "Mayo",
+                            "5": "Junio",
+                            "6": "Julio",
+                            "7": "Agosto",
+                            "8": "Septiembre",
+                            "9": "Octubre"
+                        },
+                        "weekdays": [
+                            "Domingo",
+                            "Lunes",
+                            "Martes",
+                            "Miércoles",
+                            "Jueves",
+                            "Viernes",
+                            "Sábado"
+                        ]
+                    },
+                    "editor": {
+                        "close": "Cerrar",
+                        "create": {
+                            "button": "Nuevo",
+                            "title": "Crear Nuevo Registro",
+                            "submit": "Crear"
+                        },
+                        "edit": {
+                            "button": "Editar",
+                            "title": "Editar Registro",
+                            "submit": "Actualizar"
+                        },
+                        "remove": {
+                            "button": "Eliminar",
+                            "title": "Eliminar Registro",
+                            "submit": "Eliminar",
+                            "confirm": {
+                                "_": "¿Está seguro que desea eliminar %d filas?",
+                                "1": "¿Está seguro que desea eliminar 1 fila?"
+                            }
+                        },
+                        "multi": {
+                            "title": "Múltiples Valores",
+                            "restore": "Deshacer Cambios",
+                            "noMulti": "Este registro puede ser editado individualmente, pero no como parte de un grupo.",
+                            "info": "Los elementos seleccionados contienen diferentes valores para este registro. Para editar y establecer todos los elementos de este registro con el mismo valor, haga click o toque aquí, de lo contrario conservarán sus valores individuales."
+                        },
+                        "error": {
+                            "system": "Ha ocurrido un error en el sistema (<a target=\"\\\" rel=\"\\ nofollow\" href=\"\\\"> Más información<\/a>)."
+                        }
+                    },
+                    "decimal": ".",
+                    "emptyTable": "No hay datos disponibles en la tabla",
+                    "zeroRecords": "No se encontraron coincidencias",
+                    "info": "Mostrando _START_ a _END_ de _TOTAL_ ",
+                    "infoEmpty": "Mostrando 0 a 0 de 0 ",
+                    "infoFiltered": "(Filtrado de _MAX_ total de )",
+                    "lengthMenu": "Mostrar _MENU_ ",
+                    "stateRestore": {
+                        "removeTitle": "Eliminar",
+                        "creationModal": {
+                            "search": "Busccar"
+                        }
+                    }
+                }
+            });
+            
+        }); 
+        
     </script>
+ 
 @endsection
