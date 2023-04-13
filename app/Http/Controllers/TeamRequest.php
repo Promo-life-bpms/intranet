15bca5d2bb8f7188f97cdfe6604ce39b8325d45d
ChaparroLukas <betitolucas123456789@gmail.com>
2023-04-13 11:58:39 -0600

    Creacion de la tabla historial en la vista historial

--- app/Http/Controllers/TeamRequest.php
+++ app/Http/Controllers/TeamRequest.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\TeamRequest as ModelsTeamRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TeamRequest extends Controller
 {
     //
 public function index(){
     $user = auth()->user();
-    
     return view('admin.team.index', compact('user'));
 
 }
 
 public function index1(){
-    return view('admin.team.record');
+    $datos = DB::table('request_team')
+            ->select('request_team.*')
+            ->orderBy('id')
+            ->get();
+    return view('admin.team.record')->with('datos', $datos);
+
+    
 }
 
 public function createTeamRequest(Request $request){
@@ -26,10 +32,15 @@ public function createTeamRequest(Request $request){
   $request_team->user_id = $request->user_id;
   $request_team->category = $request->category;
   $request_team->description = $request->description;
-  $request_team->status = $request->status;
+  $request_team->status = 'Solicitud enviada';
   $request_team->user_id = $user->id;
   $request_team->save();
   return redirect()->route('team.request');
-}
+
+ 
+  }
+
 
 }
+
+
--- resources/views/admin/Team/record.blade.php
+++ resources/views/admin/Team/record.blade.php
@@ -5,5 +5,33 @@
     <div class="card-header">
         <h1 style="font-size:20px">Historial de Solicitudes</h1>
     </div>
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped" id="table-directory">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Categoria</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">ID</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($datos as $dato )
+                    <tr>
+                        <th>{{$dato->user_id}}</th>
+                        <th>{{$dato->category}}</th>
+                        <th>{{$dato->description}}</th>
+                        <th>{{$dato->status}}</th>
+                        <th>{{$dato->id}}</th> 
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
