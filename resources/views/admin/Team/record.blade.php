5d6f2f70f1f42b9e2997bc2ab24d881884da790c
ChaparroLukas <betitolucas123456789@gmail.com>
2023-04-17 13:43:36 -0600

    Modificación en la tabla de historial de la vista solicitud de equipo

--- resources/views/admin/Team/record.blade.php
+++ resources/views/admin/Team/record.blade.php
@@ -11,11 +11,11 @@
             <table class="table table-striped" id="table-directory">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Categoria</th>
-                        <th scope="col">Descripción</th>
-                        <th scope="col">Estado</th>
-                        <th scope="col">ID</th>
+                        <th scope="col" style="text-align: center">N° Usuario</th>
+                        <th scope="col" style="text-align: center">Categoria</th>
+                        <th scope="col" style="text-align: center">Descripción</th>
+                        <th scope="col" style="text-align: center">Estado</th>
+                        <th scope="col" style="text-align: center">ID</th>
                     </tr>
                 </thead>
 
@@ -25,13 +25,13 @@
                         @if($dato->user_id === auth()->id())
                             <input type="hidden" {{$dato->user_id}}>
                             <tr>
-                                <th>{{$dato->user_id}}</th>             
+                                <th style="text-align: center">{{$dato->user_id}}</th>             
                                 <th>{{$dato->category}}</th>
                                 <th>{{$dato->description}}</th>
                                 <th>{{$dato->status}}</th>
                                 <th>{{$dato->id}}</th>
-                            </tr>                  
-                        @endif                        
+                            </tr>
+                        @endif 
                     @endforeach
                 </tbody>
             </table>
