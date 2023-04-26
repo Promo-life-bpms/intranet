dcc3b1eb497f0a00881d175d924bc3f1686a7538
ChaparroLukas <betitolucas123456789@gmail.com>
2023-04-26 15:06:57 -0600

    Nueva modificación en la vista de detalles del usuario

--- resources/views/systems/show.blade.php
+++ resources/views/systems/show.blade.php
@@ -3,7 +3,15 @@
 @section('content')
 <div class="container">
     <div class="card-header">
-        <h1 style="font-size:20px">Detalles del Usuario</h1>
+        <div class="d-flex justify-content-between">
+            <div class="d-flex flex-row">
+                <a  href="{{ route('systems.request')}}">
+                    <i class="fa fa-arrow-left fa-2x arrouw-back" aria-hidden="true"></i>
+                </a>
+                <h1  style="margin-left:16px; font-size:25px" class="separator">Detalles del Usuario</h1> 
+            </div>
+        </div>
+    </div>
     
 <!--body-->
     <div class="imagen-centrada">
@@ -23,26 +31,22 @@
 
                 @csrf
                         <div class="button-container">
-                            <textarea name="comment" id="comment"></textarea>   
-                        </div>
-                        <br>
+                            <textarea name="comments" id="comment"></textarea>   
                         
-                        <div class="row">
-                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <div>
-                                            <a type="button" class="btn btn-success"  href="{{ route('systems.request')}}">Regresar</a>
+                                <div class="col-md-4">
+                                        <div class="form-group">
+                                            <div>
+                                            
+                                            </div>
                                         </div>
-                                    </div>
 
-                            <div class="col-md-6">
-                                    <div class="form-group">
-                                            {!! Form::select('status', ['Aceptar'=> 'Aceptar', 'Rechazar'=> 'Rechazar'], 'Estado', ['class' => 'form-control','placeholder' => 'Seleccione el estado']) !!}
-                                    </div>
+                                <div class="col-md-6">
+                                        <div class="form-group">
+                                                {!! Form::select('status', ['Aceptar'=> 'Aceptar', 'Rechazar'=> 'Rechazar'], 'Estado', ['class' => 'form-control','placeholder' => 'Seleccione el estado']) !!}
+                                        </div>
                                 </div>
-                            </div>
-                                            {!! Form::submit('ACTUALIZAR', ['class' => 'btnCreate mt-4']) !!}
                         </div>
+                        {!! Form::submit('ACTUALIZAR', ['class' => 'btnCreate mt-4']) !!}
             </form>       
 </div>
         
@@ -60,5 +64,8 @@
     max-width: 100%;
     max-height: 100%;
 }
+    h1{
+        text-align: 10%;
+    }
 </style>
 @endsection
