a2874894f5ca91fa504997d4108a1b3d371c7a67
ChaparroLukas <betitolucas123456789@gmail.com>
2023-05-23 14:28:44 -0600

    Avance de formulario de solicitudes

--- resources/views/admin/Team/index.blade.php
+++ resources/views/admin/Team/index.blade.php
@@ -15,9 +15,9 @@
 
     <form action="{{route('team.createTeamRequest')}}" method="POST">
 
-                {!! Form::open(['route' => 'team.request', 'enctype' => 'multipart/form-data']) !!}
-                <h2 style="font-size: 15px;">Datos Generales del Personal de Nuevo Ingreso</h2>
-            @csrf
+            {!! Form::open(['route' => 'team.request', 'enctype' => 'multipart/form-data']) !!}
+                <h2 style="font-size: 20px;">Datos Generales del Personal de Nuevo Ingreso</h2>
+                 @csrf
                     <div class="row">
                         <div class="col-md-4">
                             <div class="form-group">
@@ -55,6 +55,201 @@
                             </div>
                         </div>
 
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Área: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el área']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Departamento: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el departamento']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Puesto: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el puesto']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Extensión: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la extensión']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Jefe inmediato: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese a jefe inmediato']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Empresa: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la empresa']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Gerente de área: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese a gerente de área']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                <h2 style="font-size: 20px;">Asignación de Equipo de Cómputo y Telefonía</h2>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Tipo de computadora: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el tipo de computadora']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Celular: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el celular']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', '#: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el número']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'No. de extensión: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el No. de extensión']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Equipo a utilizar: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el equipo a utilizar']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Accesorios: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el accesorio']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'En caso de ser reasignación de equipo indique el usuario anterior: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el usuario anterior']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+                
+                <h2 style="font-size: 20px;">Cuenta(s) de Correo(s) Requerida(s)</h2>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Correo: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el correo']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('', 'Firma: ') !!}
+                                {!! Form::text('', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el correo']) !!}
+                                @error('')
+                                <small>
+                                    <font color="red"> *Este campo es requerido* </font>
+                                </small> 
+                                @enderror
+                            </div>
+                        </div>
                     </div>
     </div>
         {!! Form::submit('CREAR SOLICITUD', ['class' => 'btnCreate mt-4']) !!}
