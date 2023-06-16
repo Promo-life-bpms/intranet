dfccac70873cafb68404a933f75a57854b9213a5
ChaparroLukas <betitolucas123456789@gmail.com>
2023-06-16 13:26:01 -0600

    Avance en la vista administracion de solicitudes, colocación de tabla para recibir datos mandados por el usuario

--- resources/views/admin/Team/admon.blade.php
+++ resources/views/admin/Team/admon.blade.php
@@ -5,5 +5,31 @@
     <div class="card-header">
         <h1 style="font-size:20px">Administración de Solicitudes</h1>
     </div>
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped" id="table-directory">
+                <thead>
+                    <tr>
+                        <th scope="col" style="text-align: center">ID de Solicitud</th>
+                        <th scope="col" style="text-align: center">Solicitante</th>
+                        <th scope="col" style="text-align: center">Estado</th>
+                        <th scope="col" style="text-align: center">Fecha de creación</th>
+                        <th scope="col" style="text-align: center">Opciones</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 </div>
+
+<style>
+    table {
+    font-size: 66.1%;
+    }
+
+    .btn {
+    font-size: 10px;
+    }
+</style>
 @endsection
